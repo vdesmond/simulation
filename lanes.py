5fcdb9d3cc72727605e5298e697b92c5a509caef
beanboi7 <sr7ramwashere@protonmail.com>
2021-10-01 20:55:50 +0530

    code cleanup

--- lanes.py
+++ lanes.py
@@ -4,13 +4,15 @@ import numpy as np
 global_left_fit_average = []
 global_right_fit_average = []
 
+
 def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
     y1 = int(image.shape[0])
-    y2 = int(y1 * (1/2))
-    x1 = int((y1 - intercept)/ slope)
-    x2 = int((y2 - intercept)/ slope)
-    return np.array([x1,y1,x2,y2])
+    y2 = int(y1 * (1 / 2))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2, y2])
+
 
 def average_slope_intercept(image, lines):
     left_fit = []
@@ -19,14 +21,14 @@ def average_slope_intercept(image, lines):
     global global_right_fit_average
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
-        parameters = np.polyfit((x1, x2), (y1,y2), 1)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
         slope = parameters[0]
         intercept = parameters[1]
         if slope < 0:
             left_fit.append((slope, intercept))
         else:
             right_fit.append((slope, intercept))
-    if (len(left_fit) == 0):
+    if len(left_fit) == 0:
         left_fit_average = global_left_fit_average
     else:
         left_fit_average = np.average(left_fit, axis=0)
@@ -38,53 +40,48 @@ def average_slope_intercept(image, lines):
     right_line = make_coordinates(image, right_fit_average)
     return np.array([left_line, right_line])
 
+
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    canny = cv2.Canny(blur, 50 ,100)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 100)
     return canny
+
 
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
         for x1, y1, x2, y2 in lines:
-            cv2.line(line_image, (x1,y1), (x2,y2), (255,0,0), 10)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
+
 
 def region_of_interest(image):
     height = image.shape[0]
-    polygons = np.array([ [(200,height), (1100, height), (550,250)] ])
+    polygons = np.array([[(200, height), (1100, height), (550, 250)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
-# image = cv2.imread('test_image.jpg')
-# lane_image = np.copy(image)
-# canny_image = canny(lane_image)
-# cropped_image = region_of_interest(canny_image)
-# lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap =5)
-# averaged_lines = average_slope_intercept(lane_image, lines)
-# line_image = display_lines(lane_image, averaged_lines)
-# final_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 
-#cv2.imwrite('averaged_final_image.jpg', final_image)
-
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 60, (1280,720))
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter("output.avi", fourcc, 60, (1280, 720))
 
 cap = cv2.VideoCapture("test2.mp4")
-while(cap.isOpened()):
+while cap.isOpened():
     ret, frame = cap.read()
     canny_image = canny(frame)
     cropped_image = region_of_interest(canny_image)
-    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap =5)
+    lines = cv2.HoughLinesP(
+        cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5
+    )
     averaged_lines = average_slope_intercept(frame, lines)
     line_image = display_lines(frame, averaged_lines)
     final_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
     out.write(final_image)
-    cv2.imshow('output', final_image)
-    if cv2.waitKey(1) == ord('q'):
+    cv2.imshow("output", final_image)
+    if cv2.waitKey(1) == ord("q"):
         break
 
 cap.release()
